2dd028a284bb8f63144ae7e6705161e1901f2141
ms9212 <perso07.ms@gmail.com>
2022-02-08 17:44:11 +0900

    Initial commit -m "인텔리제이로 int string"

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+        int number = 5;
+        System.out.println(number);
+
+        String sparta = "Hello Sparta";
+        System.out.println(sparta);
+
+        final int finalNumber = 1;
+        System.out.println(finalNumber);
+        sparta = "Good Bye Sparta";
+        System.out.println(sparta);
+    }
+}
